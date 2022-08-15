f64747bafd0ddf26c5a79a8223139a3f98da5bb0
Diego_Flores <ldiego.flores98@gmail.com>
2022-08-15 16:32:12 -0700

    pConfiguracion de webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import '../styles/global.scss';
 
 const App = () => {
   return (
-    <div>Hola Mundo!</div>
-  )
+    <div>Hola Mundo!, ¿Es mejor AI, IoT o 3D desingn?</div>
+  );
 }
 
-export default App
+export default App;
 
